23dbc0134682e196440e66133a4e41ce76d0d575
Alexey Loubyansky <olubyans@redhat.com>
2016-12-16 10:38:12 +0100

    Next is 2.0.1.Alpha4

--- patch-gen-maven-plugin/src/main/java/org/jboss/as/patch/generator/maven/plugin/PatchGenMojo.java
+++ patch-gen-maven-plugin/src/main/java/org/jboss/as/patch/generator/maven/plugin/PatchGenMojo.java
@@ -49,7 +49,7 @@ import org.jboss.as.patching.generator.PatchGenerator;
  * <plugin>
  *     <groupId>org.jboss.as</groupId>
  *     <artifactId>patch-gen-maven-plugin</artifactId>
- *     <version>2.0.1.Alpha3</version>
+ *     <version>2.0.1.Alpha4-SNAPSHOT</version>
  *     <executions>
  *         <execution>
  *             <id>create-patch-file</id>
